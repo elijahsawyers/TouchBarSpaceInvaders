79473b8894e863177878ede6c0ee9b7d7887a9e5
Elijah Sawyers <elijahsawyers@gmail.com>
2021-01-05 16:48:43 -0600

    Created Xcode project

---
+++ Touch Bar Space Invaders/Shared/TouchBarSpaceInvadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchBarSpaceInvadersApp.swift
+//  Shared
+//
+//  Created by Elijah Sawyers on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchBarSpaceInvadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TouchBarSpaceInvadersView()
+        }
+    }
+}
---
+++ Touch Bar Space Invaders/Shared/TouchBarSpaceInvadersView.swift
@@ -0,0 +1,15 @@
+//
+//  TouchBarSpaceInvadersView.swift
+//  Shared
+//
+//  Created by Elijah Sawyers on 1/5/21.
+//
+
+import SwiftUI
+
+struct TouchBarSpaceInvadersView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
